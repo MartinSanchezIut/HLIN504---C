e1ad293b7482e1ee7aed5f5a497e549d95de37a2
Martin <martin.sanchez@etu.umontpellier.fr>
2020-10-02 17:14:28 +0200

    TP2

---
+++ TP2/mm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define TAILLE 4
+
+typedef struct mm* mm ;
+
+struct mm
+{
+	char secret[TAILLE+1] ;
+	int nbEssai ;
+};
+
+
+
+mm mm_creer() {
+	mm jeu = malloc(sizeof(TAILLE+1) + sizeof(int)) ;
+
+	for (int i = 0; i < TAILLE ; ++i)
+	{
+		jeu*+i =   '2' ; //rand() % 10;
+	}
+
+	return jeu;
+}
+
+
+
+
+void mm_detruire(mm* mm) {
+	free(mm) ;
+}
+

61e02e26f811bb012e33264b807148ae0419fb14
Martin <martin.sanchez@etu.umontpellier.fr>
2020-11-13 12:48:16 +0100

    Updates

--- TP6/RecursiviteFichier.c
+++ TP6/RecursiviteFichier.c
@@ -15,31 +15,25 @@ void recursiv(char* path, int prefix) {
     struct dirent *dirent;
     DIR *dir;
     
-    for (int i = 0; i < prefix; ++i){
+    for (int i = 0; i < prefix-1; ++i){
     	printf(" ");
     }
-    printf("%s \n", path);
+    printf("|%s \n", path);
 
 	dir=opendir(path);
     if (dir != NULL) {
-
     	while ((dirent = readdir(dir)) != NULL) {
     		if (strncmp(dirent->d_name, "..", 2)) {
     			if (strncmp(dirent->d_name, ".", 1)) {
     				recursiv(dirent->d_name, prefix +4);
-    				//printf("Printable %s\n", dirent->d_name);
     			}
     		}
-    		/*
-    		if (dirent->d_name != "." && dirent->d_name != "..") {
-
-    			printf("Printable %s\n", dirent->d_name);
-    			//recursiv(dirent->d_name, prefix +1);
-    		}
-			*/
     	}
-
+    	closedir(dir);
 	}
+	//printf("Ca marche pas\n");
+
+
 }
 
 int main(int argc, char const *argv[]){
